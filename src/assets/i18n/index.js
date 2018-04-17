0c02c878148e1e200cec1847e4c3656b45143e2c
Ashley Yang <Ashley_Yang@trend.com.tw>
2018-04-17 16:54:15 +0800

    Update gh-pages.

--- src/assets/i18n/index.js
+++
@@ -1,1 +0,0 @@
-// empty, it's just needed to point the loader to the root directory of the locales
--- src/containers/footer/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-    return (<footer>
-        All Copyright Preserved 2018
-    </footer>);
-};
-
-export default Footer;
